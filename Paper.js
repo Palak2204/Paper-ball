d1ddeb932d7ca82b04be5b5caedd3331cb1f19dd
Palak2204 <82875691+Palak2204@users.noreply.github.com>
2021-05-24 08:20:33 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,13 @@
+class Paper extends PaperClass{
+    constructor(x,y){
+      super(x,y,90,90);
+      this.image = loadImage("paper.png");
+    }
+    display(){
+       
+      super.display();
+    }
+  }
+
+
+
